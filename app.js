288c633c46bb7831a50a5c86c4078f160adee822
ssasmi <ssasmi@gmail.com>
2022-05-26 21:49:16 +0300

    color app

---
+++ app.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("color");
+const color = document.querySelector(".color");
+
+console.log(btn, color);
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
